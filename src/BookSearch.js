042a9b044be00e85847f8993ece78eaae339f5f7
snhwang <snhwang@alum.mit.edu>
2017-08-20 20:31:25 -0500

    Changed to a stateless functional component

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -2,32 +2,21 @@
 
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types'
 
-class BookSearch extends Component {
-  static propTypes = {
-    onSearch: PropTypes.func.isRequired
-  }
-
-  render() {
-    return(
-      <div>
-        <form className="book-search-form">
-          <div className="search-details">
-            <input
-              className='search-term'
-              type="text"
-              placeholder="Seach terms"
-              value={this.props.query}
-              autoFocus
-              onChange={(event) => this.props.onSearch(event.target.value)}
-            />
-            <Link className="close-book-search" to="/">Close</Link>
-          </div>
-        </form>
+export const BookSearch = ({ onSearch, query }) =>
+  <div>
+    <h2>Search Component</h2>
+    <form className="book-search-form">
+      <div className="search-details">
+        <input
+          value={query}
+          autoFocus
+          className='search-term'
+          type="text"
+          placeholder="Seach terms"
+          onChange={(event) => onSearch(event.target.value)}
+        />
+        <Link className="close-book-search" to="/">Close</Link>
       </div>
-    )
-  }
-}
-
-export default BookSearch
+    </form>
+  </div>
